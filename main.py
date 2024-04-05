fe653e0bc9d7211f9351d71c4cb161da287063e9
dariatk <157084033+dariatk@users.noreply.github.com>
2024-04-04 18:49:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    num1 = float(request.form['num1'])
-    num2 = float(request.form['num2'])
-    operation = request.form['operation']
+@app.route('/')
+def index():
+    return 'Hello, World'
 
-    if operation == 'add':
-        result = num1 + num2
-    elif operation == 'subtract':
-        result = num1 - num2
-    elif operation == 'multiply':
-        result = num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return 'Error: Division by zero is not allowed', 400
-        result = num1 / num2
-    else:
-        return 'Error: Invalid operation', 400
-
-    return 'Result: {}'.format(result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(host='0.0.0.0', port=8000)
